34ff3ddebb216b29bc50a6e7a8061369bc86b911
Brenner87 <alk870501@gmail.com>
2017-01-27 01:56:43 +0200

    FitApp the first steps

---
+++ FitApp.py
@@ -0,0 +1,32 @@
+import sqlite3, os
+
+def create_db(db_name):
+	cur_dir=os.getcwd()
+	create_query='CREATE TABLE products(name VARCHAR PRIMARY KEY, protein REAL, fat REAL, carbohydrate REAL, kcal REAL)'
+	try:
+		con=sqlite3.connect(db_name)
+		cur=con.cursor()
+		cur.execute(create_query)
+		con.commit()
+	except Exception:
+		print("Was not able to create DB file in "+cur_dir+"\\"+db_name)
+
+def insert_data(db_name,values):
+	insert_query='INSERT INTO products VALUES (?,?,?,?,?)'
+	#try:
+	con=sqlite3.connect(db_name)
+	cur=con.cursor()
+	cur.execute(insert_query, (values['name'], values['protein'], values['fat'], values['carbohydrate'], values['kcal'],))
+	con.commit()
+	#except Exception:
+	#	print("Was not able to insert prduct "+values['name'])
+	#else:
+	#	print(values[name]+" was successfully inserted")
+
+db_name='Products.db'
+
+if not os.path.isfile(db_name): create_db(db_name)
+insert_data(db_name,{'name':'Творог', 'protein':17, 'fat':5, 'carbohydrate':0,'kcal':80})
+
+
+
